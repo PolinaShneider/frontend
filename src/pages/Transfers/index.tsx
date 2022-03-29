366658a78f105330c773db94003570d8c749d9fa
a-girl-who-codes <beatrixlinkoln@gmail.com>
2022-03-29 13:25:42 +0300

    Make transfer

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { OptionShape, Select } from '@alfalab/core-components/select';
 import authHeader from "../../utils";
+import { Input } from "@alfalab/core-components/input";
+import { Button } from "@alfalab/core-components/button";
 
 const BACKEND_URL = 'http://94.228.120.208';
 
@@ -19,6 +21,7 @@ type OnSelectProps = {
 
 export const Transfers = () => {
     const [balance, setUserBalance] = React.useState(0);
+    const [amountToTransfer, setAmountToTransfer] = React.useState(0);
     const [options, setOptions] = React.useState([] as OptionShape[]);
     const [recipientId, setRecipientId] = React.useState('');
 
@@ -38,19 +41,35 @@ export const Transfers = () => {
         }).then((result) => {
             const data = result.data.response;
             if (data) {
-                setOptions(data.map((item: UserOption) => { 
+                setOptions(data.map((item: UserOption) => {
                     return {
                         key: item.id, content: item.name
                     }
-                 }))
+                }))
             }
         })
     }
-    const handleChange = ({selected}: OnSelectProps) => {
+    const handleChange = ({ selected }: OnSelectProps) => {
         if (selected) {
-            setRecipientId(selected.value);
+            setRecipientId(selected.key);
         }
     };
+
+    const handleAmount = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setAmountToTransfer(+value);
+    }
+
+    const makeTransfer = () => {
+        axios.post(`${BACKEND_URL}/operations/transfer`, {
+            paymentAmount: amountToTransfer,
+            incomingAccount: recipientId,
+        }, { 
+            headers: authHeader() 
+        }).then(() => {
+            getUserBalance()
+        })
+    }
 
     React.useEffect(() => {
         getUserBalance();
@@ -68,6 +87,11 @@ export const Transfers = () => {
                     placeholder="id получателя"
                     onChange={handleChange}
                 />
+                <Input
+                    placeholder="Сколько отправляем?"
+                    onChange={handleAmount}
+                />
+                <Button onClick={makeTransfer}>Перевести</Button>
             </div>
         </div>
     )
