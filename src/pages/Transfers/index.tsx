4a39ccb7e67f36dcbbfdbdc4eb64903f3f513702
a-girl-who-codes <beatrixlinkoln@gmail.com>
2022-03-29 13:05:52 +0300

    Select logic

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -1,14 +1,26 @@
 import axios from "axios";
 import React from "react";
-import { Select } from '@alfalab/core-components/select';
+import { OptionShape, Select } from '@alfalab/core-components/select';
 import authHeader from "../../utils";
 
 const BACKEND_URL = 'http://94.228.120.208';
 
+type UserOption = {
+    name: string;
+    id: string;
+}
+
+type OnSelectProps = {
+    selected: OptionShape | null;
+    selectedMultiple: OptionShape[];
+    initiator: OptionShape | null;
+    name?: string;
+}
+
 export const Transfers = () => {
     const [balance, setUserBalance] = React.useState(0);
-    const [options, setOptions] = React.useState([]);
-    const [receiverId, setReceiverId] = React.useState(null);
+    const [options, setOptions] = React.useState([] as OptionShape[]);
+    const [recipientId, setRecipientId] = React.useState('');
 
     const getUserBalance = () => {
         axios.get(`${BACKEND_URL}/user/me`, {
@@ -20,12 +32,29 @@ export const Transfers = () => {
             }
         })
     };
-    const handleChange = () => {
-        
+    const getOtherUsers = () => {
+        axios.get(`${BACKEND_URL}/user/usersWithoutMe`, {
+            headers: authHeader(),
+        }).then((result) => {
+            const data = result.data.response;
+            if (data) {
+                setOptions(data.map((item: UserOption) => { 
+                    return {
+                        key: item.id, content: item.name
+                    }
+                 }))
+            }
+        })
+    }
+    const handleChange = ({selected}: OnSelectProps) => {
+        if (selected) {
+            setRecipientId(selected.value);
+        }
     };
 
     React.useEffect(() => {
         getUserBalance();
+        getOtherUsers();
     }, [])
     return (
         <div>
@@ -38,7 +67,6 @@ export const Transfers = () => {
                     options={options}
                     placeholder="id получателя"
                     onChange={handleChange}
-                    selected={receiverId}
                 />
             </div>
         </div>
