3fad57ac0ae3d4bd2453d3d860fdfd2e7425882f
a-girl-who-codes <beatrixlinkoln@gmail.com>
2022-03-29 12:45:22 +0300

    Select initial

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import React from "react";
+import { Select } from '@alfalab/core-components/select';
 import authHeader from "../../utils";
 
 const BACKEND_URL = 'http://94.228.120.208';
 
 export const Transfers = () => {
     const [balance, setUserBalance] = React.useState(0);
+    const [options, setOptions] = React.useState([]);
+    const [receiverId, setReceiverId] = React.useState(null);
 
     const getUserBalance = () => {
         axios.get(`${BACKEND_URL}/user/me`, {
@@ -17,6 +20,10 @@ export const Transfers = () => {
             }
         })
     };
+    const handleChange = () => {
+        
+    };
+
     React.useEffect(() => {
         getUserBalance();
     }, [])
@@ -24,8 +31,16 @@ export const Transfers = () => {
         <div>
             <h1>Страница переводов</h1>
             <p>
-            Ваш баланс: <span>{balance} бонусов</span> 
+                Ваш баланс: <span>{balance} бонусов</span>
             </p>
+            <div>
+                <Select
+                    options={options}
+                    placeholder="id получателя"
+                    onChange={handleChange}
+                    selected={receiverId}
+                />
+            </div>
         </div>
     )
 }
